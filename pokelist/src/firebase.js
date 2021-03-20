d99bcc8626ba916dc4a61468cc68a9d016e3cdd1
felipemimoura <felipemimoura@gmail.com>
2021-03-20 11:04:21 -0300

    configuraçao inicial do react + firebase

---
+++ pokelist/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyCdN4-Pk9tc_z6W-6lDCkstK8twODWuC74",
+  authDomain: "lista-pokemon-6ca4a.firebaseapp.com",
+  projectId: "lista-pokemon-6ca4a",
+  storageBucket: "lista-pokemon-6ca4a.appspot.com",
+  messagingSenderId: "962846569134",
+  appId: "1:962846569134:web:87a5f630e8fea9292db33f",
+  measurementId: "G-78BYND8014",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
